3127e1193820e517fe1f897e86b2f10528a1d0e9
Pololoshka <sokolova.polya.98gmail.com>
2024-07-09 02:35:59 +0300

    Added few queries in query.sql

--- dags/dds_layer/sql/create_schema.sql
+++ dags/dds_layer/sql/create_schema.sql
@@ -1,53 +1,55 @@
-CREATE SCHEMA IF NOT EXISTS "{{ params.schema_name }}";
+BEGIN;
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".scills_group (
-	id INT PRIMARY KEY,
-	scill_group_name VARCHAR(50) NOT NULL,
-    UNIQUE (scill_group_name)
+CREATE SCHEMA IF NOT EXISTS "dds_polina";
+
+CREATE TABLE IF NOT EXISTS "dds_polina".skills_group (
+	id SERIAL PRIMARY KEY,
+	skill_group_name VARCHAR(50) NOT NULL,
+    UNIQUE (skill_group_name)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".levels (
-	id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS "dds_polina".levels (
+	id SERIAL PRIMARY KEY,
 	"level" VARCHAR(50) NOT NULL,
     level_num INT NOT NULL,
-    UNIQUE ("level", level_num)
+    UNIQUE (level_num)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".departments (
+CREATE TABLE IF NOT EXISTS "dds_polina".departments (
 	id SERIAL PRIMARY KEY,
 	department VARCHAR(50) NOT NULL,
     UNIQUE (department)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".position (
+CREATE TABLE IF NOT EXISTS "dds_polina".position (
 	id SERIAL PRIMARY KEY,
 	position VARCHAR(50) NOT NULL,
     UNIQUE (position)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".departments_position (
+CREATE TABLE IF NOT EXISTS "dds_polina".departments_position (
 	id SERIAL PRIMARY KEY,
 	dep_id INT NOT NULL,
     pos_id INT NOT NULL,
     CONSTRAINT fk_departments_position_departments
         FOREIGN KEY(dep_id)
-            REFERENCES "{{ params.schema_name }}".departments(id),
+            REFERENCES "dds_polina".departments(id),
     CONSTRAINT fk_departments_position_position
         FOREIGN KEY(pos_id)
-            REFERENCES "{{ params.schema_name }}".position(id)
+            REFERENCES "dds_polina".position(id)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".skills (
+CREATE TABLE IF NOT EXISTS "dds_polina".skills (
 	id SERIAL PRIMARY KEY,
     skill_name VARCHAR(50) NOT NULL,
 	group_id INT NOT NULL,
-    CONSTRAINT fk_skills_scills_group
+    CONSTRAINT fk_skills_skills_group
         FOREIGN KEY(group_id)
-            REFERENCES "{{ params.schema_name }}".scills_group(id),
+            REFERENCES "dds_polina".skills_group(id),
     UNIQUE (skill_name, group_id)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".employees (
+CREATE TABLE IF NOT EXISTS "dds_polina".employees (
 	id INT PRIMARY KEY,
     last_name VARCHAR(50),
     first_name VARCHAR(50),
@@ -56,13 +58,13 @@ CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".employees (
     pos_id INT NOT NULL,
     CONSTRAINT fk_employees_departments
         FOREIGN KEY(dep_id)
-            REFERENCES "{{ params.schema_name }}".departments(id),
+            REFERENCES "dds_polina".departments(id),
     CONSTRAINT fk_employees_position
         FOREIGN KEY(pos_id)
-            REFERENCES "{{ params.schema_name }}".position(id)
+            REFERENCES "dds_polina".position(id)
 );
 
-CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".skills_levels (
+CREATE TABLE IF NOT EXISTS "dds_polina".skills_levels (
 	id SERIAL PRIMARY KEY,
     "date" DATE NOT NULL,
     skill_num INT NOT NULL,
@@ -72,12 +74,14 @@ CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}".skills_levels (
     employee_id INT NOT NULL,
     CONSTRAINT fk_skills_levels_skills
         FOREIGN KEY(skill_id)
-            REFERENCES "{{ params.schema_name }}".skills(id),
+            REFERENCES "dds_polina".skills(id),
     CONSTRAINT fk_skills_levels_levels
         FOREIGN KEY(level_id)
-            REFERENCES "{{ params.schema_name }}".levels(id),
+            REFERENCES "dds_polina".levels(id),
     CONSTRAINT fk_skills_levels_employees
         FOREIGN KEY(employee_id)
-            REFERENCES "{{ params.schema_name }}".employees(id),
+            REFERENCES "dds_polina".employees(id),
     UNIQUE (skill_id, level_id, employee_id)
 );
+
+COMMIT;
