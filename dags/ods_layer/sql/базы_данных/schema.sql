cc9612d15311506350c1b8896936e66ea4a19e59
Pololoshka <sokolova.polya.98gmail.com>
2024-07-09 19:33:37 +0300

    Formatted SQL

--- dags/ods_layer/sql/базы_данных/schema.sql
+++ dags/ods_layer/sql/базы_данных/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}"."{{ params.table_name }}" (
-	название varchar(50) NULL,
-	активность varchar(50) NULL,
-	"Сорт." int4 NULL,
-	"Дата изм." varchar(50) NULL,
-	id int4 NULL
+  название varchar(50) NULL,
+  активность varchar(50) NULL,
+  "Сорт." int4 NULL,
+  "Дата изм." varchar(50) NULL,
+  id int4 NULL
 );
--- dags/ods_layer/sql/технологии_и_уровень_знаний_сотру/schema.sql
+++ dags/ods_layer/sql/технологии_и_уровень_знаний_сотру/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS "{{ params.schema_name }}"."{{ params.table_name }}" (
-	название text NULL,
-	активность text NULL,
-	"Сорт." int4 NULL,
-	"Дата изм." text NULL,
-	id int4 NULL,
-	дата text NULL,
-	технологии text NULL,
-	"Уровень знаний" text NULL
+  название text NULL,
+  активность text NULL,
+  "Сорт." int4 NULL,
+  "Дата изм." text NULL,
+  id int4 NULL,
+  дата text NULL,
+  технологии text NULL,
+  "Уровень знаний" text NULL
 );
